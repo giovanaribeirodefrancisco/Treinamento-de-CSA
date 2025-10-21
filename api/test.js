490a80fa3bbb1fb6f2f9a456b2f81207eeb5a01d
Giovana Ribeiro de Francisco <161640729+giovanaribeirodefrancisco@users.noreply.github.com>
2025-10-21 08:14:48 -0300

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,7 +1,7 @@
-module.exports = (req, res) => {
+/*module.exports = (req, res) => {
   res.status(200).json({ 
     message: 'API funcionando!',
     method: req.method,
     timestamp: new Date().toISOString()
   });
-};
+};*/
